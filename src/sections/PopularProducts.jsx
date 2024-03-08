67f13619fc6e0819efb26f934176838a6d374f10
alienfoxx <alienfoxx@gmail.com>
2024-03-08 04:36:03 +0100

    finshing the popular product section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,4 +1,4 @@
-import {PopularProductCard} from "../components/"
+import PopularProductCard from "../components/PopularProductCard"
 import { products } from "../constants"
 
 const PopularProducts=() =>{
@@ -11,7 +11,7 @@ const PopularProducts=() =>{
             </div>
             <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-14">
                 {products.map((product) => (
-                    <PopularProductCard key={product.name} {...product}/>
+                    <PopularProductCard key={product.name}{...product}/>
                 )) 
                 }
 
